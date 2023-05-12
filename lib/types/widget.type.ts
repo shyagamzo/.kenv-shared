c2d2f9e3742d33d54324756b78d0621af2ecd779
Shy Agam <shyagam@gmail.com>
2023-05-12 13:46:17 +0300

    ♻️ Replaced types with Zod schema to allow validations when moving data between scripts

--- lib/types/widget.type.ts
+++ lib/types/widget.type.ts
@@ -1,30 +1,37 @@
 import type { WidgetMessage } from '@johnlindquist/kit/types/pro';
+import { z } from 'zod';
 
 export type DropFile = { name: string, path: string, size: number, type: string, lastModified: string, lastModifiedDate: Date, webkitRelativePath: string };
 export type DropImage = DropFile;
+export type DropVideo = DropFile;
+export type DropAudio = DropFile;
+export type DropApp = DropFile;
 export type DropText = string;
 export type DropUri = string;
 export type DropHtml = string;
 export type DropOther = string | Record<string, unknown>;
 
-export type WidgetDropCategory = 'files' | 'texts' | 'uris' | 'htmls' | 'images' | 'others';
+export type WidgetDropCategory = 'files' | 'images' | 'videos' | 'apps' | 'texts' | 'uris' | 'htmls' | 'others';
 
 export type WidgetDropItem<Info, Mime extends string> = { info: Info, mime: Mime };
 
-export type WidgetDropItemCategorized<Category extends WidgetDropCategory, Info, Mime extends string> = { [k in Category]: WidgetDropItem<Info, Mime>[] }
+export const DroppedItemSchema = <InfoSchema extends z.Schema, MimeSchema extends z.ZodString | z.ZodLiteral<string>>(info: InfoSchema, mime: MimeSchema) => z.array(z.object({ info, mime }));
+export const DroppedFileItemSchema = <MimeSchema extends z.ZodString | z.ZodLiteral<string>>(mime: MimeSchema) => DroppedItemSchema(z.object({ name: z.string(), path: z.string(), size: z.number(), type: z.string(), lastModified: z.number(), lastModifiedDate: z.string().datetime(), webkitRelativePath: z.string() }), mime);
 
-export type WidgetDropTypesMap = {
-    files: WidgetDropItem<DropFile, ''>,
-    images: WidgetDropItem<DropImage, `image/${ string }`>,
-    texts: WidgetDropItem<DropText, `text/plain`>,
-    uris: WidgetDropItem<DropUri, `text/uri${ string }`>,
-    htmls: WidgetDropItem<DropHtml, `text/html${ string }`>,
-    others: WidgetDropItem<DropOther, string>
-};
+export const DroppedDataSchema = z.object({
+    files: DroppedFileItemSchema(z.literal('')),
+    images: DroppedFileItemSchema(z.string().regex(/^image\/.*$/)),
+    videos: DroppedFileItemSchema(z.string().regex(/^video\/.*$/)),
+    audios: DroppedFileItemSchema(z.string().regex(/^audio\/.*$/)),
+    apps: DroppedFileItemSchema(z.string().regex(/^application\/.*$/)),
 
-export type WidgetDropTypeInfo<Category extends WidgetDropCategory> = WidgetDropTypesMap[Category]['info'];
-export type WidgetDropTypeMime<Category extends WidgetDropCategory> = WidgetDropTypesMap[Category]['mime'];
+    texts: DroppedItemSchema(z.string(), z.literal('text/plain')),
+    uris: DroppedItemSchema(z.string(), z.string().regex(/^text\/uri.*$/)),
+    htmls: DroppedItemSchema(z.string(), z.string().regex(/^text\/html.*$/)),
 
-export type CategorizedDropItems = { [k in keyof WidgetDropTypesMap]?: WidgetDropTypesMap[k][] };
+    others: DroppedItemSchema(z.unknown(), z.string())
+}).partial();
+
+export type CategorizedDropItems = z.infer<typeof DroppedDataSchema>;
 
 export type WidgetDropMessage = WidgetMessage & { dataTransfer?: CategorizedDropItems; };
