3ff4939d8fc6122c8f648b6077bb6a725616b618
Shy Agam <us@bespunky.io>
2023-07-06 20:37:37 +0300

    Implemented non-streaming open-ai completion

--- lib/open-ai.ts
+++ lib/open-ai.ts
@@ -1,6 +1,6 @@
 import '@johnlindquist/kit';
 
-import { Configuration, CreateChatCompletionRequest, OpenAIApi } from 'openai';
+import { Configuration, CreateChatCompletionRequest, CreateChatCompletionResponse, OpenAIApi } from 'openai';
 import { Observer } from 'rxjs';
 
 export async function initOpenAI(): Promise<OpenAIApi>
@@ -19,6 +19,7 @@ export interface ChatCompletionParams
 {
     prompt: string;
     systemPrompt?: string;
+    stream?: false;
 }
 
 export interface ChatCompletionState
@@ -27,37 +28,55 @@ export interface ChatCompletionState
     fullText: string;
 }
 
-type CompletionObserver = Partial<Omit<Observer<ChatCompletionState>, 'complete'>> & { complete: (fullText: string) => void };
+type CompletionObserver = Partial<Omit<Observer<ChatCompletionState>, 'complete'>> & { complete?: (fullText: string) => void };
 
-export async function createChatCompletionStream(params: ChatCompletionParams, { next, error, complete }: CompletionObserver): Promise<void>
+function shouldStreamCompletion(params: ChatCompletionParams): boolean
 {
-    const openAI = await initOpenAI();
+    return params.stream === undefined;
+}
 
+function createCompletionRequestConfig(params: ChatCompletionParams): CreateChatCompletionRequest
+{
     const messages: CreateChatCompletionRequest['messages'] = [{ role: 'user', content: params.prompt }];
 
     if (params.systemPrompt)
         messages.unshift({ role: 'system', content: params.systemPrompt });
+    
+    return {
+        messages,
+        model: 'gpt-3.5-turbo',
+        n: 1,
+        ...(shouldStreamCompletion(params) ? { stream: true } : {}),
+    };
+}
 
-    try
+function createCompletionRequestOptions(params: ChatCompletionParams): { responseType: 'stream' } | undefined
+{
+    return shouldStreamCompletion(params) ? { responseType: 'stream' } : undefined;
+}
+
+function extractCompletionRequestData(params: ChatCompletionParams, data: CreateChatCompletionResponse, observer: CompletionObserver): Promise<string>
+{
+    if (!shouldStreamCompletion(params))
     {
-        const { data, request } = await openAI.createChatCompletion({
-            messages,
-            model: 'gpt-3.5-turbo',
-            max_tokens: 1000,
-            n: 1,
-            stream: true,
-        }, { responseType: 'stream' });
+        const content = data.choices[0].message?.content ?? '';
+        
+        observer.next?.({ currentPart: content, fullText: content });
+        observer.complete?.(content);
 
+        return Promise.resolve(content);
+    }
+
+    return new Promise((resolve, reject) =>
+    {
         const dataStreamer = data as any;
 
-        let fullContent = '';
+        let fullText = '';
 
-        // If the user presses escape or closes the app, abort the request
-        // This avoids the request to continue running in the background or a ui that keeps popping up
-        onExit(() => request.abort());
-
-        dataStreamer.on('data', async data =>
+        debugger;
+        dataStreamer.on('data', async (data: { toString: () => string; }) =>
         {
+            debugger;
             const lines = data.toString().split('\n').filter(line => line.trim() !== '');
 
             for (const line of lines)
@@ -71,30 +90,50 @@ export async function createChatCompletionStream(params: ChatCompletionParams, {
                     const parsedResponse = JSON.parse(message);
                     const content = parsedResponse.choices[0].delta['content'];
 
-                    content && next({ currentPart: content, fullText: fullContent += content });
+                    fullText += content ?? '';
+
+                    content && observer.next?.({ currentPart: content, fullText });
                 } catch (err)
                 {
-                    console.log('Could not JSON parse stream message', message, err);
-                    const errorDetails = new Error('Could not JSON parse OpenAI stream message');
-
-                    errorDetails['textualResponse'] = message;
-                    errorDetails['originalError'] = err;
-
-                    error(errorDetails);
+                    reportError(err, 'Could not JSON parse OpenAI stream message');
                 }
             }
         });
 
-        dataStreamer.on('end', () => complete(fullContent));
-    }
-    catch (err)
-    {
-        const errorDetails = new Error('OpenAI stream failed.');
+        dataStreamer.on('end', () =>
+        {
+            observer.complete?.(fullText);
+            resolve(fullText);
+        });
 
-        errorDetails['originalError'] = err;
+        dataStreamer.on('error', reportError);
 
-        error(errorDetails);
-    }
+        
+        function reportError(err: unknown, message?: string)
+        {
+            const errorDetails = new Error(message ?? 'OpenAI stream failed.');
+
+            (errorDetails as any)['originalError'] = err;
+
+            observer.error?.(errorDetails);
+            reject(errorDetails);
+        }
+    });
+}
+
+export async function createChatCompletionStream(params: ChatCompletionParams, observer?: CompletionObserver): Promise<string>
+{
+    const openAI = await initOpenAI();
+    const { data, request } = await openAI.createChatCompletion(
+        createCompletionRequestConfig(params),
+        createCompletionRequestOptions(params)
+    );
+
+    // If the user presses escape or closes the app, abort the request
+    // This avoids the request to continue running in the background or a ui that keeps popping up
+    onExit(() => request.abort());
+    
+    return extractCompletionRequestData(params, data, observer ?? {});
 }
 
 export async function createTranscription(audioFile: File): Promise<string>
